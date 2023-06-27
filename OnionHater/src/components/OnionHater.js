e3244bb135eb429780dd0efd9586723f7658ebe3
NataliaChavarria <nataliachm97@gmail.com>
2023-06-27 16:15:15 -0500

    create OnionHater

---
+++ OnionHater/src/components/OnionHater.js
@@ -0,0 +1,8 @@
+export const handleChange = (event) => {
+    const text = event.target.value;
+    if (text.includes('cebolla')) {
+        alert('ODIO LA CEBOLLA')
+    }
+}
+
+// export default handleChange
